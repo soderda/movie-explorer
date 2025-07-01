63225869b660135f4c59613d1a5ee773e1b5c6f7
David Söderberg <david.soderberg98@gmail.com>
2025-07-01 09:07:29 +0200

    Initial commit with base for project

---
+++ src/test/java/com/soderberg/movie_explorer/MovieExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soderberg.movie_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
